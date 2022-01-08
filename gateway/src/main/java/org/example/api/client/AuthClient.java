a228286ecff8adbf32b427d7531a294752466011
Denys Tsebro <tsebro.denis@gmail.com>
2022-01-08 09:31:15 +0200

    [HRM-4] implement working version of the gateway server
    type: code
    
    description: -

--- gateway/src/main/java/org/example/api/client/AuthClient.java
+++ gateway/src/main/java/org/example/api/client/AuthClient.java
@@ -1,5 +1,6 @@
 package org.example.api.client;
 
+import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,6 +11,6 @@ import java.util.Map;
 public interface AuthClient {
 
     @PostMapping("/api/v1/authenticate")
-    String login(@RequestHeader Map<String, String> headers, String loginForm);
+    Response login(@RequestHeader Map<String, String> headers, String loginForm);
 
 }
--- gateway/src/main/java/org/example/api/controller/GatewayController.java
+++ gateway/src/main/java/org/example/api/controller/GatewayController.java
@@ -2,11 +2,12 @@ package org.example.api.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Response;
+import lombok.RequiredArgsConstructor;
 import org.example.api.client.AuthClient;
 import org.example.api.dto.AuthDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,20 +19,20 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/gateway")
+@RequiredArgsConstructor
 public class GatewayController {
 
-    @Autowired
-    private AuthClient authClient;
+    private final AuthClient authClient;
+    private final ObjectMapper objectMapper;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @PostMapping("/login")
-    public ResponseEntity<String> login() throws JsonProcessingException {
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<HttpStatus> login() throws JsonProcessingException {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-        return ResponseEntity.ok(
-                authClient.login(headers, objectMapper.writeValueAsString(new AuthDTO("admin", "admin"))));
+        Response response = authClient.login(headers, objectMapper.writeValueAsString(new AuthDTO("admin", "admin")));
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization", response.headers().get("Authorization").stream().findFirst().orElseThrow());
+        return ResponseEntity.status(response.status()).headers(httpHeaders).build();
     }
 
 }
